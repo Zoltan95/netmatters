6abb5ff4c5395cf08586c08dc477b63e19d3ea9b
Zoltan Urban <56049513+Zoltan95@users.noreply.github.com>
2023-07-13 07:50:07 +0100

    Fix for Sticky Header
    Animation now work perfectly with page scroll, and content doesn't jump anymore.

---
+++ js/stickyheader.js
@@ -0,0 +1,30 @@
+const headerSticky = document.getElementById("header-sticky");
+const header = document.getElementById("header");
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll <= 0) {
+        headerSticky.classList.remove("sticky");
+        $('#header-sticky').css('display', 'none') 
+        return;
+    }
+
+    if (currentScroll > lastScroll) 
+    {
+        // down 
+        $('#header-sticky').css('display', 'block')
+        headerSticky.classList.add("slide-in");
+        headerSticky.classList.remove("slide-out");
+
+    } 
+    else if (currentScroll < lastScroll) 
+    {
+        // up 
+        headerSticky.classList.add("sticky");
+        headerSticky.classList.add("slide-out");
+        headerSticky.classList.remove("slide-in");
+        
+    }
+    lastScroll = currentScroll;
+});
